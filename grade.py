c76a63d3d67d754480f51323f55a140557f7aa4a
Shlok8906 <shlokpanchal2020@gmail.com>
2024-10-14 22:28:03 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,9 @@
+a=float(input("enter you percentage="))
+if(a>90):
+    print("your grade is A")
+elif(a>80 and a<=90):
+    print("your grade is B")
+elif(a>=60 and a<=80):
+    print("your grade is C")
+else:
+    print("your grade is D")
